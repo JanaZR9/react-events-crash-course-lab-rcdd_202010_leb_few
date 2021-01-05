d9695c2e747b272e91a10a70c740a506be6a33e4
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-05 19:55:04 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -24,6 +24,17 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
   
+  handleKey = (e) => {
+    const plus = '+';
+    const minus = '-';
+    const k = e.key ;
+    if (k==='a'){
+
+    return resize(plus)}
+     else {if (k === 's') 
+     return resize(minus)}
+     
+  }
   render() {
     return (
       <canvas 
